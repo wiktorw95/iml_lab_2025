6b72b94c63a7a76cb8f928a63c001466f23b7537
Myroslav Natalchenko <99477515+MyroslavNatalchenko@users.noreply.github.com>
2025-10-02 13:31:18 +0200

    Lab1 s30023 (#6)
    * Lab 1: Podstawy Pythona i narzędzi ML
    
    * Lab 1: Podstawy Pythona i narzędzi ML
    
    * Lab1
    
    * Lab1_f
    
    * Lab1_final_clean
    
    * Lab1_final_clean

---
+++ s30023/01/basic.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    name_surname = "Myroslav Natalchenko"
+
+    numbers = [i for i in range(1, 6)]
+
+    for number in numbers:
+        print(number)
+
+    def sum_of_two_numbers(number1, number2):
+        return number1 + number2
+
+    print(sum_of_two_numbers(1, 2))
